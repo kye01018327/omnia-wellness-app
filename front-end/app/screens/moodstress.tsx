163b3273431b2143a7c6f9ae595ef13a1f2b942f
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-25 19:35:24 -0800

    feat: add placeholder workout and moodstress pages for modal

--- front-end/app/screens/moodstress.tsx
+++ front-end/app/screens/moodstress.tsx
@@ -1,11 +1,11 @@
 import { Text, Button, View } from 'react-native'
 import { useRouter } from 'expo-router'
 
-export default function WorkoutPage() {
+export default function MoodStressPage() {
     const router = useRouter()
     return (
         <View>
-            <Text>This is a placeholder Workout page</Text>
+            <Text>This is a placeholder Mood/Stress page</Text>
             <Button title='Save Entry' onPress={() => {router.replace('../(tabs)/home')}}/>
         </View>
     )
